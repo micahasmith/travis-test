54a30cf7124cb3b9e35b32eae3f51a0f95e3284f
unknown <MSmith@BE6774.BEDOMAIN.BEAP.com>
2012-04-30 19:33:57 -0400

    lessened the rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,10 +1,3 @@
-begin
-  require 'jasmine'
-  load 'jasmine/tasks/jasmine.rake'
-rescue LoadError
-  task :jasmine do
-    abort "Jasmine is not available. In order to run jasmine, you must: (sudo) gem install jasmine"
-  end
+task :default do 
+	
 end
-
-task :default => ['jasmine:ci']
